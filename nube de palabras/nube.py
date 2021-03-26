4019f7bb31bb77d12edab5e387d731ec6ac56340
mascarilla <64045193+boomcrash@users.noreply.github.com>
2021-03-26 09:22:28 -0500

    Add files via upload

---
+++ nube de palabras/nube.py
@@ -0,0 +1,10 @@
+from wordcloud import WordCloud, STOPWORDS
+import os
+comentarios=''
+stopwords=set(STOPWORDS)
+direccion=os.getcwd()
+file=open(direccion+'\\desktop\\proyectos\\nube de palabras\\file.txt',"r+")
+textdata=file.read().replace('\n','')
+wordcloud=WordCloud(width=800,height=800,background_color='white',stopwords=stopwords,min_font_size=10).generate(textdata)
+wordcloud.to_file(direccion+'\\desktop\\proyectos\\nube de palabras\\Image.png')
+print("se guardo la imagen")
